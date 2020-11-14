fa612d4ffb4ce0f9344da12feb951fb6793d9461
akhopkar01 <aadi0110@gmail.com>
2020-11-14 11:28:04 -0500

    Added Agent information

--- data_process.py
+++ data_process.py
@@ -1,19 +1,39 @@
 from l5kit.rasterization import build_rasterizer
 from l5kit.configs import load_config_data
-from l5kit.data import LocalDataManager, ChunkedDataset
+from l5kit.data import ChunkedDataset
 from l5kit.dataset import AgentDataset
 import numpy as np
 
-zarr_dt = ChunkedDataset("/home/majoradi/Documents/l5-sample/sample.zarr")
+# Declare global variables
+zarr_dt = ChunkedDataset("/home/majoradi/Documents/l5_sample/sample.zarr")
 zarr_dt.open()
+AGENTS = zarr_dt.agents
+FRAMES = zarr_dt.frames
+SCENES = zarr_dt.scenes
+datasetSize = 5
+cols = ["frame_id", "object_id", "object_type", "posx", "posy", "posz", "velx", "vely", "length", "width", "height",
+        "heading"]
+dataset_dict = {}
+dataset_list = []
+for col in cols:
+    dataset_dict[col] = 0
 
-# additional information is required for rasterisation
-cfg = load_config_data("~/")
-rast = build_rasterizer(cfg, LocalDataManager("/tmp/l5kit_data"))
 
-# create a mask where an agent every 100th is set to True
-agents_mask = np.zeros(len(zarr_dt.agents), dtype=np.bool)
-agents_mask[np.arange(0, len(agents_mask), 100)] = True
+# ADDING AGENT INFORMATION
+def addAgentInformation():
+    for idx in range(datasetSize):
+        dataset_dict["posx"], dataset_dict["posy"] = AGENTS[idx][0]
+        dataset_dict["length"], dataset_dict["width"], dataset_dict["height"] = AGENTS[idx][1]
+        dataset_dict["heading"] = AGENTS[idx][2]
+        dataset_dict["velx"], dataset_dict["vely"] = AGENTS[idx][3]
+        dataset_dict["object_id"] = AGENTS[idx][4]  # Not sure
+        label_probabilities = np.array(AGENTS[idx][5])
+        obj_type = np.where(label_probabilities == 1)[0] + 1  # Not sure
+        dataset_dict["object_type"] = obj_type[0]
+        dataset_list.append(dataset_dict)
+    print(dataset_dict)
+    return dataset_list
 
-
-dataset = AgentDataset(cfg, zarr_dt, rast, agents_mask=agents_mask)
+# ADDING FRAME INFORMATION TO THE LIST
+def addFrameInformation():
+    return dataset_list
