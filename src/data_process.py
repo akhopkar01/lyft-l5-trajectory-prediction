f3a2293a0391cb2add3cfe00aa95734763b3f30a
kartik venkat <kartik.venkat86@gmail.com>
2020-11-17 08:34:52 -0500

    - Updated Data Process

--- src/data_process.py
+++ src/data_process.py
@@ -7,7 +7,7 @@ from l5kit.dataset import AgentDataset, EgoDataset
 
 from l5kit.rasterization import build_rasterizer
 from l5kit.configs import load_config_data
-
+from l5kit.data.labels import PERCEPTION_LABELS
 
 import numpy as np
 import os
@@ -57,23 +57,84 @@ def loadDataset(TYPE):
 #%%
 
 # ADDING AGENT INFORMATION
-def addAgentInformation(AGENTS, FRAMES, SCENES):
+def addAgentInformation(AGENTS):
+    '''
+    :param AGENTS: All the agents in dataset in zarr format
+    :return: list of dictionaries (each containing all information in a frame).
+    '''
+
+    '''
+    
+    dataset_dict = { posx : x coordinate of agent , 
+                     posy : y coordinate of agent ,
+                     length : length of agent  ,
+                     width : width of agent , 
+                     height : height of agent ,
+                     heading : heading of agent , 
+                     velx : x-axis velocity of agent , 
+                     vely : y-axis velocity of agent ,
+                     object_id : object id of agent to keep track of agent, 
+                     object_type : type of object (car,bus,bicycle,... etc) }
+                     
+    dataset_list = [dataset_dict, dataset_dict, ... ] // len: datasetSize
+    
+    '''
+    agent_dict = {}
+    agent_list = []
     for idx in range(datasetSize):
-        dataset_dict["posx"], dataset_dict["posy"] = AGENTS[idx][0]
-        dataset_dict["length"], dataset_dict["width"], dataset_dict["height"] = AGENTS[idx][1]
-        dataset_dict["heading"] = AGENTS[idx][2]
-        dataset_dict["velx"], dataset_dict["vely"] = AGENTS[idx][3]
-        dataset_dict["object_id"] = AGENTS[idx][4]  # Not sure
+        agent_dict["posx"], agent_dict["posy"] = AGENTS[idx][0]
+        agent_dict["length"], agent_dict["width"], agent_dict["height"] = AGENTS[idx][1]
+        agent_dict["heading"] = AGENTS[idx][2]
+        agent_dict["velx"], agent_dict["vely"] = AGENTS[idx][3]
+        agent_dict["object_id"] = AGENTS[idx][4]
         label_probabilities = np.array(AGENTS[idx][5])
-        obj_type = np.where(label_probabilities == 1)[0] + 1  # Not sure
-        dataset_dict["object_type"] = obj_type[0]
-        dataset_list.append(dataset_dict)
-    print(dataset_dict)
-    return dataset_list
+        obj_type = np.where(label_probabilities == 1)[0][0]
+        agent_dict["object_type"] = PERCEPTION_LABELS[obj_type]
+        agent_list.append(agent_dict)
+
+    return agent_list
 
 # ADDING FRAME INFORMATION TO THE LIST
-def addFrameInformation():
-    return dataset_list
+def addFrameInformation(AGENTS, FRAMES, SCENES):
+    '''
+    :param FRAMES: All the frames in dataset in zarr format
+    :return: list of dictionaries (each containing all information in a frame).
+    '''
+
+    '''
+
+    dataset_dict = { posx : x coordinate of agent , 
+                     posy : y coordinate of agent ,
+                     length : length of agent  ,
+                     width : width of agent , 
+                     height : height of agent ,
+                     heading : heading of agent , 
+                     velx : x-axis velocity of agent , 
+                     vely : y-axis velocity of agent ,
+                     object_id : object id of agent to keep track of agent, 
+                     object_type : type of object (car,bus,bicycle,... etc) }
+
+    dataset_list = [dataset_dict, dataset_dict, ... ] // len: datasetSize
+
+    '''
+    agent_dict = {}
+    cols = ["frame_id", "object_id", "object_type", "posx", "posy", "posz", "velx", "vely", "length", "width", "height",
+            "heading"]
+    for col in cols:
+        agent_dict[col] = 0
+    agent_list = []
+    for idx in range(datasetSize):
+        agent_dict["posx"], agent_dict["posy"] = AGENTS[idx][0]
+        agent_dict["length"], agent_dict["width"], agent_dict["height"] = AGENTS[idx][1]
+        agent_dict["heading"] = AGENTS[idx][2]
+        agent_dict["velx"], agent_dict["vely"] = AGENTS[idx][3]
+        agent_dict["object_id"] = AGENTS[idx][4]
+        label_probabilities = np.array(AGENTS[idx][5])
+        obj_type = np.where(label_probabilities == 1)[0][0]
+        agent_dict["object_type"] = PERCEPTION_LABELS[obj_type]
+        agent_list.append(agent_dict)
+
+    return agent_list
 
 #%%
 '''
@@ -85,11 +146,11 @@ def generateTrainDataset(train_zarr):
     FRAMES = train_zarr.frames
     AGENTS = train_zarr.agents
 
-    # print("SCENES:\n",SCENES[0])
-    # print("FRAMES:\n",FRAMES[0])
-    print("AGENTS:\n",AGENTS[0])
+    # print("SCENES:\n",SCENES.size)
+    print("FRAMES:\n",FRAMES[0])
+    print("AGENTS:\n",AGENTS.size)
 
-    train_dataset_list = addAgentInformation(AGENTS, FRAMES, SCENES)
+    train_dataset_list = addAgentInformation(AGENTS)
 
     print("Train dataset list:\n", train_dataset_list)
 
@@ -107,7 +168,7 @@ def generateValDataset(val_zarr):
     # print("FRAMES:\n", FRAMES[0])
     print("AGENTS:\n", AGENTS[0])
 
-    val_dataset_list = addAgentInformation(AGENTS, FRAMES, SCENES)
+    val_dataset_list = addAgentInformation(AGENTS)
 
 #%%
 
@@ -120,12 +181,12 @@ if __name__ == '__main__':
     datasetSize = 5
     cols = ["frame_id", "object_id", "object_type", "posx", "posy", "posz", "velx", "vely", "length", "width", "height",
             "heading"]
-    dataset_dict = {}
-    dataset_list = []
-    for col in cols:
-        dataset_dict[col] = 0
+
+    # for col in cols:
+    #     dataset_dict[col] = 0
 
     train_zarr, train_dataset, train_dataloader = loadDataset("train_data_loader")
+    print("TRAIN DATASET:\n", train_dataset)
 
     val_zarr, val_dataset, val_dataloader = loadDataset("val_data_loader")
 
